56ec330ad6637ba8b8b83e0b6c8f531637fad7ab
Alex <anicalace@gmail.com>
2021-02-13 17:37:15 +0200

    lesson 12

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.pinclipart.com/picdir/big/51-511102_design-free-logo-srj-hd-logo-png-clipart.png"
+                 alt=""/>
+        </header>
+    )
+}
+
+export default Header;
